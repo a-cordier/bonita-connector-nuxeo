a24c5a034381aa1fb0cc596cb416eb815413fd3c
acordier <anton.36ve@gmail.com>
2014-07-07 09:35:03 +0200

    using StreamBlob instead of FileBlob

--- bonita-connector-nuxeo-upload-impl/src/main/java/fr/univlille2/bpm/NuxeoUploadConnector.java
+++ bonita-connector-nuxeo-upload-impl/src/main/java/fr/univlille2/bpm/NuxeoUploadConnector.java
@@ -1,8 +1,6 @@
 package fr.univlille2.bpm;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.logging.Level;
@@ -12,21 +10,25 @@ import org.bonitasoft.engine.api.ProcessAPI;import org.bonitasoft.engine.connect
 import org.nuxeo.ecm.automation.client.Session;
 import org.nuxeo.ecm.automation.client.adapters.DocumentService;
 import org.nuxeo.ecm.automation.client.model.Document;
-import org.nuxeo.ecm.automation.client.model.FileBlob;
+import org.nuxeo.ecm.automation.client.model.StreamBlob;
 
-// TODO: write definition file
+/**
+ * 
+ * @author acordier Jul 7, 2014
+ * NuxeoUploadConnector.java
+ */
 public class NuxeoUploadConnector extends NuxeoConnector {
-	
+
 	private String attachment;
 	private String path;
 	private String title;
 	private String type;
- 	@SuppressWarnings("rawtypes")
-	private ArrayList properties;
-	
+	private ArrayList<Object> properties;
+
 	private Logger logger = Logger.getLogger(this.getClass().getName());
-	
- 	@SuppressWarnings("rawtypes")
+
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public void setInputParameters(final Map<String, Object> parameters) {
 		super.setInputParameters(parameters);
@@ -34,63 +36,53 @@ public class NuxeoUploadConnector extends NuxeoConnector {
 		path = (String) parameters.get("path");
 		title = (String) parameters.get("title");
 		type = (String) parameters.get("type");
-	 	properties = (ArrayList) parameters.get("properties");
-	
+		properties = (ArrayList<Object>) parameters.get("properties");
+
 	}
 
-		
-	@SuppressWarnings("rawtypes")
 	@Override
+	@SuppressWarnings("rawtypes")
 	protected void executeConnector(final Session session) throws Exception {
-		logger.info("Starting nuxeo upload connector for document " + attachment);
-		
-		
+		if(logger.isLoggable(Level.INFO)){
+			logger.info("Starting nuxeo upload connector for a document named " + attachment);
+		}
+		// bonita document retrieval
 		ProcessAPI processAPI = getAPIAccessor().getProcessAPI();
 		long processInstanceId = getExecutionContext().getProcessInstanceId();
 		org.bonitasoft.engine.bpm.document.Document srcDocument = 
-				processAPI.getLastDocument(processInstanceId, this.attachment);	
-		
+				processAPI.getLastDocument(processInstanceId, this.attachment);			
 		if(srcDocument==null){
 			throw new ConnectorException("Unable to retrieve process a attachment referenced by " + attachment);
-		}		
+		}			
 		final byte[] content = processAPI.getDocumentContent(srcDocument.getContentStorageId());		
 		final ByteArrayInputStream inputStream = new ByteArrayInputStream(content);
 		String fileName = srcDocument.getContentFileName();
 		String mimeType = srcDocument.getContentMimeType();
-		logger.info("Mime type: " + mimeType);
+
+		// nuxeo document API call
 		DocumentService documentService = session.getAdapter(DocumentService.class);
 		org.nuxeo.ecm.automation.client.model.Document nxDocument = new Document(title, type);
-		
-		path = checkParentPath(path); // Correct path if user forgot the last file-separator
-		nxDocument.set("dc:title", title);
-		for(Object o : properties){
-			logger.info(o.getClass().getName());
-			ArrayList row = (ArrayList)o;
-		    nxDocument.set((String)row.get(0),(String)row.get(1));
-		}
-		nxDocument = documentService.createDocument(path, nxDocument);
-		
-		File tempFile = new File(fileName);
-		FileOutputStream fos = new FileOutputStream(tempFile);
 
-		int data;
-		while((data=inputStream.read())!=-1){
-			char currentChar = (char)data;
-			fos.write(currentChar);
+		path = checkParentPath(path); 
+		nxDocument.set("dc:title", title);
+		// additional metadata attachment
+		for(Object object : properties){
+			ArrayList row = (ArrayList)object;
+			nxDocument.set((String)row.get(0),(String)row.get(1));
 		}
-		fos.flush();
-		fos.close();
-		FileBlob blob = new FileBlob(tempFile); 
-		//StreamBlob blob = new StreamBlob(inputStream, fileName, mimeType);
+		// document creation
+		nxDocument = documentService.createDocument(path, nxDocument);
+		// blob attachment
+		StreamBlob blob = new StreamBlob(inputStream, fileName, mimeType);
 		documentService.setBlob(nxDocument, blob);
-		tempFile.delete();
 		documentService.update(nxDocument);
+		
 		if(logger.isLoggable(Level.INFO)){
 			logger.info(String.format("File %s sent to %s", fileName, path));
 		}
 	}
-         
-    
+
+
 	private static String checkParentPath(String path){
 		return path.charAt(path.length()-1)!='/'?path+"/":path;
 	}
