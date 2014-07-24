434571f5805e7b4ead929e7b85d06f037a4a7f54
acordier <anton.36ve@gmail.com>
2014-07-24 15:31:48 +0200

    added folder connector

---
+++ bonita-connector-nuxeo-folder-impl/src/main/java/fr/univlille2/bpm/NuxeoFolderConnector.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package fr.univlille2.bpm;
+
+import org.nuxeo.ecm.automation.client.Session;
+
+/**
+ * @author acordier Jul 24, 2014
+ * NuxeoFolderConnector.java
+ */
+public class NuxeoFolderConnector extends NuxeoConnector {
+
+	/**
+	 * 
+	 */
+	public NuxeoFolderConnector() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.univlille2.bpm.NuxeoConnector#executeConnector(org.nuxeo.ecm.automation.client.Session)
+	 */
+	@Override
+	protected void executeConnector(Session session) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
